33eed0452938c79eaed20f604727a0b0e1443ca0
Oscar-Song <osong@ucsd.edu>
2017-08-01 12:40:43 -0700

    rm sd

--- sumDay.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-DAWmat=[['Day1', 'arm', 3], ['Day1', 'arm', 2], ['Day1','leg',5],['Day1','arm',6],['Day2', 'leg', 1], ['Day2', 'arm', 4]]
-df = pd.DataFrame(DAWmat, columns=['Col1','Col2','Col3'])
-gro = df.groupby(['Col1','Col2'], as_index=False)['Col3'].sum()
-lol = gro.values.tolist()
-lol
